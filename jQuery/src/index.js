4a9edd09a402e4a142a9e492a0a509d0a91e0673
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-02-12 10:35:05 +0200

    Load index.js as module in jQuery
    (cherry picked from commit a3997a79ceb3d609779864b89f8ea357c38fafa9)

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,21 +1,12 @@
+import { count as initialCount } from "./data.js";
+
 $(() => {
-  const oDataContext = new DevExpress.data.ODataContext({
-    url: 'https://services.odata.org/Northwind/Northwind.svc',
-    errorHandler(error) {
-      DevExpress.ui.notify(error.message, 'error', 3000);
+  let count = initialCount;
+  $('#btn').dxButton({
+    text: `Click count: ${count}`,
+    onClick(e) {
+      count += 1;
+      e.component.option('text', `Click count: ${count}`);
     },
-    version: 3,
-    entities: {
-      Products: { key: 'ProductID' },
-    },
-  });
-  $('#data-grid').dxDataGrid({
-    columns: ['ProductID', 'ProductName', 'Category.CategoryID', 'Category.CategoryName'],
-    dataSource: new DevExpress.data.DataSource({
-      store: oDataContext.Products,
-      select: ['ProductID', 'ProductName', 'Category.CategoryID', 'Category.CategoryName'],
-      expand: ['Category'],
-      filter: ['Category.CategoryID', '=', 5],
-    }),
   });
 });

f2ae776214a8da166ccfa5e26906f10b0451ffd1
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 18:31:37 +0300

    Adapt template updates to the jQuery-only example

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,21 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  const oDataContext = new DevExpress.data.ODataContext({
+    url: 'https://services.odata.org/Northwind/Northwind.svc',
+    errorHandler(error) {
+      DevExpress.ui.notify(error.message, 'error', 3000);
     },
+    version: 3,
+    entities: {
+      Products: { key: 'ProductID' },
+    },
+  });
+  $('#data-grid').dxDataGrid({
+    columns: ['ProductID', 'ProductName', 'Category.CategoryID', 'Category.CategoryName'],
+    dataSource: new DevExpress.data.DataSource({
+      store: oDataContext.Products,
+      select: ['ProductID', 'ProductName', 'Category.CategoryID', 'Category.CategoryName'],
+      expand: ['Category'],
+      filter: ['Category.CategoryID', '=', 5],
+    }),
   });
 });
